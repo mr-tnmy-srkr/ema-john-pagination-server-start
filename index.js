9441759cfdb91c4aff536d52ac6e2f354583e46b
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-02 10:12:45 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // middleware
 app.use(cors());
@@ -12,7 +12,11 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = "mongodb+srv://srkr_tnmy:HcQJQ4W9c153jcxQ@myprojectscluster.drcktji.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://tnmy_srkr:877smORJET4ct5n4@myprojectscluster.drcktji.mongodb.net/?retryWrites=true&w=majority";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
